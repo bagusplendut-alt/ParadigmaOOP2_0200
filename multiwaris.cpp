a13907d4f7e404022927da1ce73d4f166556b7d0
bagusplendut-alt <bagusplendut@gmail.com>
2026-05-18 17:03:15 +0700

    tambah variabel umur dan constructor pada kelas orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -4,3 +4,10 @@ using namespace std;
 class orang
 {
 public:
+    int umur;
+    orang(int pUmur) : umur(pUmur)
+    {
+        cout << " orang di buat dengan umur " << umur << "\n"
+             << endl;
+    }
+};
